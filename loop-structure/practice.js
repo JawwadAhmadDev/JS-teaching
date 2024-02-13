0de4771d77f5869b554969db57c495d513f7666f
Jawwad Ahmad <109460336+JawwadAhmadDev@users.noreply.github.com>
2024-02-13 19:52:48 +0500

    example2

--- loop-structure/practice.js
+++ loop-structure/practice.js
@@ -1,5 +1,16 @@
-let i = 0;
-while (i < 50) {
-  console.log(i);
-  i++;
+// let i = 0;
+// while (i < 50) {
+//   console.log(i);
+//   i++;
+// }
+
+let someArray = ["Mike", "Antal", "Marc", "Emir", "Louiza", "Jacky"];
+let notFound = true;
+while (notFound && someArray.length > 0) {
+  if (someArray[0] === "Louiza") {
+    console.log("Found her!");
+    notFound = false;
+  } else {
+    someArray.shift();
+  }
 }
