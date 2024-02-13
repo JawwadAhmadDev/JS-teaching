633997c5acffd84164b0696318829ecff4a2dbad
Jawwad Ahmad <109460336+JawwadAhmadDev@users.noreply.github.com>
2024-02-13 19:58:09 +0500

    fibonacci

--- loop-structure/practice.js
+++ loop-structure/practice.js
@@ -4,13 +4,29 @@
 //   i++;
 // }
 
-let someArray = ["Mike", "Antal", "Marc", "Emir", "Louiza", "Jacky"];
-let notFound = true;
-while (notFound && someArray.length > 0) {
-  if (someArray[0] === "Louiza") {
-    console.log("Found her!");
-    notFound = false;
-  } else {
-    someArray.shift();
-  }
+// let someArray = ["Mike", "Antal", "Marc", "Emir", "Louiza", "Jacky"];
+// let notFound = true;
+// while (notFound && someArray.length > 0) {
+//   if (someArray[0] === "Louiza") {
+//     console.log("Found her!");
+//     notFound = false;
+//   } else {
+//     someArray.shift();
+//   }
+// }
+
+// fabonacci series
+let nr1 = 0;
+let nr2 = 1;
+let temp;
+
+let fibonacciArray = [];
+
+while (fibonacciArray.length < 25) {
+  fibonacciArray.push(nr1);
+  temp = nr1 + nr2; // 2
+  nr1 = nr2; // 1
+  nr2 = temp; // 2
 }
+
+console.log(fibonacciArray);
